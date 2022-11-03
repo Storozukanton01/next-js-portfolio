0c0202b3f36745cf1b91593d4354e15d2f874e23
Storozukanton <storozukanton01@gmail.com>
2022-11-04 00:58:42 +0200

    19

--- server/index.js
+++ server/index.js
@@ -8,17 +8,63 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const data = {
+  portfolios: [
+    {
+      _id: '24525',
+      title: 'Job in USA',
+      content: 'contentt',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: '24526',
+      title: 'Job in Barcelona',
+      content: 'contentt',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: '24527',
+      title: 'Job in UA',
+      content: 'contentt',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+  ]
+}
+
 app.prepare().then(() => {
   const server = express()
   const schema = buildSchema(`
+    type Portfolio {
+      _id: ID
+      title: String!
+      content: String
+      jobTitle: String
+      daysOfExperience: Int
+      isCurrentlyEmployed: Boolean
+    }
+
     type Query {
       hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
     }
   `)
 
   const root = {
     hello: () => {
       return 'Hello world!'
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
     }
   }
 
